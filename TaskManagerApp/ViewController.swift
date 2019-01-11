dd9a7fe9e4265e2c74dbc9415ba1b155372d25e7
chrisbraunschweiler1 <cbraunschweiler@gmail.com>
2019-01-11 06:16:10 +0100

    Initial Commit

---
+++ TaskManagerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaskManagerApp
+//
+//  Created by Chris Braunschweiler on 11.01.19.
+//  Copyright © 2019 braunsch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
